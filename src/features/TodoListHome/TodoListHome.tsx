efdf484a29d6ecf3854bf94221e6444cb167c8da
sangbi <dltkdqls9603@naver.com>
2025-08-20 23:45:39 +0900

    TodoList 작업완료 ++

--- src/features/TodoListHome/TodoListHome.tsx
+++ src/features/TodoListHome/TodoListHome.tsx
@@ -1,76 +1,140 @@
 import theme from "@/styles/theme";
 import {
-	Button,
-	Container,
-	Stack,
-	Table,
-	TableBody,
-	TableContainer,
-	TextField,
-	Typography,
-	useMediaQuery,
+  Button,
+  Checkbox,
+  Container,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+  useMediaQuery,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoListTableRow from "./components/TodoListTableRow";
 
 export default function TodoListHome() {
-	const isDesktop = useMediaQuery(theme.breakpoints.up("md"), {
-		defaultMatches: true,
-	});
-	const [input, setInput] = useState<string>("");
-	const [datas, setDatas] = useState<string[]>([]);
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"), {
+    defaultMatches: true,
+  });
+  const [input, setInput] = useState<string>("");
+  const [datas, setDatas] = useState<string[]>([]);
+  const [selChecks, setSelChecks] = useState<number[]>([]);
+  const [selAll, setSelAll] = useState<boolean>(false);
 
-	// 데이터 추가
-	const handleClickTodoInsert = () => {
-		if (input === "") {
-			return alert("빈 데이터는 추가할 수 없습니다.");
-		}
-		const newArr = [...datas, input];
-		setDatas(newArr);
-		setInput("");
-	};
+  const handleInsert = () => {
+    if (input === "") {
+      return alert("빈 데이터는 추가할 수 없습니다.");
+    }
+    const newArr = [...datas, input];
+    setDatas(newArr);
+    setInput("");
+  };
 
-	return (
-		<Stack
-			sx={{ alignItems: "center", justifyContent: "center" }}
-			flex={1}
-			spacing={1}>
-			<Typography variant="h4">TodoList</Typography>
-			<Container
-				sx={{
-					backgroundColor: "#555",
-					p: 1,
-					borderRadius: 2,
-				}}>
-				<Stack spacing={2}>
-					{/* input */}
-					<Stack direction={"row"} justifyContent={"center"} spacing={1}>
-						<TextField
-							type="search"
-							sx={{ maxWidth: isDesktop ? "600px" : "none" }}
-							fullWidth
-							placeholder="할 일을 입력해주세요"
-							size="small"
-							value={input}
-							onChange={(e) => setInput(e.target.value)}
-							onKeyDown={(e) => {
-								if (e.key === "Enter") handleClickTodoInsert();
-							}}
-						/>
-						<Button onClick={handleClickTodoInsert}>추가</Button>
-					</Stack>
-					{/* result */}
-					<TableContainer sx={{ backgroundColor: "#777" }}>
-						<Table>
-							<TableBody>
-								{datas.map((el, idx) => {
-									return <TodoListTableRow key={idx} data={el} />;
-								})}
-							</TableBody>
-						</Table>
-					</TableContainer>
-				</Stack>
-			</Container>
-		</Stack>
-	);
+  const handleUpdate = (selIdx: number, selData: string) => {
+    const newArr = [...datas];
+    newArr.splice(selIdx, 1, selData);
+    setDatas(newArr);
+  };
+
+  const handleDelete = () => {
+    if (selChecks.length === 0) return alert("삭제할 항목을 선택하세요.");
+    if (!confirm("선택한 항목을 삭제하시겠습니까?")) return;
+    setDatas(datas.filter((_, i) => !selChecks.includes(i)));
+    setSelChecks([]);
+    setSelAll(false);
+  };
+
+  const handleCheck = (selIdx: number, checked: boolean) => {
+    if (checked) {
+      setSelChecks((prev) => [...prev, selIdx]);
+    } else {
+      setSelChecks((prev) => prev.filter((el) => el !== selIdx));
+    }
+  };
+
+  useEffect(() => {
+    if (selAll) {
+      setSelChecks(Array.from({ length: datas.length }, (_, i) => i));
+    } else {
+      setSelChecks([]);
+    }
+  }, [selAll, datas]);
+
+  return (
+    <Stack
+      sx={{ alignItems: "center", justifyContent: "center" }}
+      flex={1}
+      spacing={1}
+    >
+      <Typography variant="h4">TodoList</Typography>
+      <Container
+        sx={{
+          backgroundColor: "#555",
+          p: 1,
+          borderRadius: 2,
+        }}
+      >
+        <Stack spacing={2}>
+          {/* input */}
+          <Stack direction={"row"} justifyContent={"center"} spacing={1}>
+            <TextField
+              type="search"
+              sx={{ maxWidth: isDesktop ? "600px" : "none" }}
+              fullWidth
+              placeholder="할 일을 입력해주세요"
+              size="small"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleInsert();
+              }}
+            />
+            <Button onClick={handleInsert}>추가</Button>
+          </Stack>
+          {/* result */}
+          <TableContainer sx={{ backgroundColor: "#777" }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{ py: 1 }}>
+                    <Checkbox
+                      size="small"
+                      checked={selAll}
+                      onClick={() => setSelAll((p) => !p)}
+                    />
+                  </TableCell>
+                  <TableCell sx={{ width: "100%" }}></TableCell>
+                  <TableCell sx={{ py: 1 }}>
+                    <Button onClick={handleDelete} size="small">
+                      선택삭제
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {datas.map((el, idx) => {
+                  return (
+                    <TodoListTableRow
+                      key={idx}
+                      seq={idx}
+                      data={el}
+                      selected={selChecks.includes(idx)}
+                      onClickUpdate={handleUpdate}
+                      onClickCheck={handleCheck}
+                      isDesktop={isDesktop}
+                    />
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Stack>
+      </Container>
+    </Stack>
+  );
 }
--- src/features/TodoListHome/components/TodoListTableRow.tsx
+++ src/features/TodoListHome/components/TodoListTableRow.tsx
@@ -1,22 +1,89 @@
-import { IconButton, TableCell, TableRow } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
+import SaveIcon from "@mui/icons-material/Save";
+import {
+  Checkbox,
+  IconButton,
+  TableCell,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
 interface Props {
-	data: string;
+  seq: number;
+  data: string;
+  selected: boolean;
+  onClickUpdate: (selIdx: number, selData: string) => void;
+  onClickCheck: (selIdx: number, checked: boolean) => void;
+  isDesktop: boolean;
 }
 export default function TodoListTableRow(props: Props) {
-	const { data } = props;
-	return (
-		<TableRow>
-			<TableCell sx={{ width: "100%" }}>{data}</TableCell>
-			<TableCell sx={{ minWidth: 100, py: 0 }}>
-				<IconButton size="small">
-					<EditIcon />
-				</IconButton>
-				<IconButton size="small">
-					<DeleteIcon />
-				</IconButton>
-			</TableCell>
-		</TableRow>
-	);
+  const { seq, data, selected, onClickUpdate, onClickCheck, isDesktop } = props;
+  const [uptMode, setUptMode] = useState<boolean>(false);
+  const [uptData, setUptData] = useState<string>(data);
+
+  const handleClickUptInstBtn = () => {
+    if (uptMode) {
+      if (uptData === "") {
+        return alert("빈데이터로 수정할 수 없습니다.");
+      }
+      if (!confirm("수정하시겠습니까?")) return;
+      onClickUpdate(seq, uptData);
+    }
+    setUptMode((p) => !p);
+  };
+
+  const handleClickCloseBtn = () => {
+    if (!confirm("취소하시겠습니까? 수정된 내용은 저장되지 않습니다.")) return;
+    setUptMode(false);
+    setUptData(data);
+  };
+
+  return (
+    <TableRow>
+      <TableCell sx={{ py: 0 }}>
+        <Checkbox
+          size="small"
+          checked={selected}
+          onChange={() => {
+            onClickCheck(seq, !selected);
+          }}
+        />
+      </TableCell>
+      <TableCell sx={{ width: "100%", py: uptMode ? 1 : undefined }}>
+        {uptMode ? (
+          <TextField
+            sx={{ maxWidth: isDesktop ? "100%" : undefined }}
+            fullWidth
+            size="small"
+            value={uptData}
+            onChange={(e) => {
+              setUptData(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                if (!confirm("수정하시겠습니까?")) return;
+                onClickUpdate(seq, uptData);
+                setUptMode((p) => !p);
+              }
+            }}
+          />
+        ) : (
+          <Typography>{data}</Typography>
+        )}
+      </TableCell>
+      <TableCell sx={{ minWidth: 100, py: 0 }}>
+        <IconButton size="small" onClick={handleClickUptInstBtn}>
+          {uptMode ? <SaveIcon /> : <EditIcon />}
+        </IconButton>
+        {uptMode && (
+          <IconButton size="small" onClick={handleClickCloseBtn}>
+            <CloseIcon />
+          </IconButton>
+        )}
+      </TableCell>
+    </TableRow>
+  );
 }
