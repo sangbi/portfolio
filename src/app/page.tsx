6a3f7c2a1f043ed99b6606cf1e16c2a2b2bac605
sangbi <dltkdqls9603@naver.com>
2025-08-22 14:31:26 +0900

    경로 상대경로로 임시 수정 ++

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 
 import dynamic from "next/dynamic";
 
-const MainHome = dynamic(() => import("@/features/MainHome"), {
-	ssr: false,
-	loading: () => <p>Loading...</p>,
+const MainHome = dynamic(() => import("../features/MainHome"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
 });
 
 export default function HomePage() {
-	return <MainHome />;
+  return <MainHome />;
 }
