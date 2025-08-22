36b7145bb84d5e036481462a8fad41f0dd17b0af
sangbi <dltkdqls9603@naver.com>
2025-08-22 13:30:06 +0900

    next.config.ts 환경설정 파일 수정(build시 out 디렉토리 생성) ++

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // StrictMode 끔
+  output: "export", // 정적 사이트 export
+  basePath: "/portfolio", // GitHub 저장소 이름과 맞춤
+  images: {
+    unoptimized: true, // Pages에서 이미지 최적화 비활성화
+  },
 };
 
 export default nextConfig;
