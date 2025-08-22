ea9b7f12ffbe5c31a853cc88c957761ac49f62e5
sangbi <dltkdqls9603@naver.com>
2025-08-22 14:43:14 +0900

    빌드 설정파일 수정 ++

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   reactStrictMode: false, // StrictMode 끔
-  output: "export", // 정적 사이트 export
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
   images: {
     unoptimized: true, // Pages에서 이미지 최적화 비활성화
   },
